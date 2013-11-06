8adc05b2af37ac0390f73e07192f5e5751f879d9
ANNOITG <andre.norberglindhe@it-gymnasiet.se>
2013-11-06 13:15:54 +0100

    Kommenterade Program.cs
    Kommenterade utförligt Program.cs filen för er inlärning. Lite
    förklaringar med mera. =)

--- OOP_Example_12TE/OOP_Example_12TE/Program.cs
+++ OOP_Example_12TE/OOP_Example_12TE/Program.cs
@@ -11,23 +11,25 @@ namespace OOP_Example_12TE
         static void Main(string[] args)
         {
 
-            List<Person> myPersons = new List<Person>();
+            List<Person> myPersons = new List<Person>(); //Listan med personer. Denna lista skapas när programmet startar.
 
             while (true)
             {
 
-                Console.WriteLine("Tryck A för att lägga till en ny person\n"
+                Console.WriteLine("Tryck A för att lägga till en ny person\n" //Lite alternativ för användaren. Lägg till, Skriv ut och Avsluta
                                 + "Tryck B för att skriva ut alla i listan\n"
                                 + "Tryck X för att avsluta");
 
-                string val = Console.ReadLine().ToLower();
+                string val = Console.ReadLine().ToLower(); //Läs in valet användaren gör och spara i variabeln "val".
 
                 switch (val)
                 {
                     case "a":
 
-                        Person per = new Person();
+                        Person per = new Person(); //Skapa en variabel "per" (Person per) som fylls av ett nytt objekt av klassen Person ( = new Person() )
 
+
+                        //Nedan fylles obejtet "per" på av användaren.
                         Console.WriteLine("Personnummer?");
                         per.pnr = Console.ReadLine();
 
@@ -38,30 +40,29 @@ namespace OOP_Example_12TE
                         per.adress = Console.ReadLine();
 
 
-                        myPersons.Add(per);
+                        myPersons.Add(per); //Obejtet "per" läggs till i listan myPersons. (Nu är listan inte tom längre)
                         
                         break;
 
                     case "b":
-                        Console.WriteLine("Antal skapade persons: " + Person.counter);
+                        Console.WriteLine("Antal skapade Personer: " + Person.counter); //Anropar den statiska variabeln i klassen Personer (public static int counter)
 
-                        foreach (Person p in myPersons)
+                        foreach (Person p in myPersons) //Loopar ut alla objekt av klassen Person som finns i listan myPersons. Varje objekt kallas för "p"
                         {
-                            Console.WriteLine(p + "\n");
+                            Console.WriteLine(p + "\n"); //Skriver ut data med hjälp av ToString()-metoden som vi skrivit i klassen Person
                         }
                         break;
 
                     case "x":
-                        Environment.Exit(0);
+                        Environment.Exit(0); //Avsluter programmet med koden 0
                         break;
 
                     default:
                         Console.WriteLine("Du gjorde inte ett korrekt val.");
                         Console.Clear();
                         break;
-                }
-            }
-            Console.ReadKey();
-        }
-    }
-}
+                } //<---- Switch end
+            }//<----- Loope end
+        }//<----- Main(string[] args) end
+    }//<---- Class Program end
+}//<--- namespace end
